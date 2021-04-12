d0afb6ee61bd5fabc74767da79d5f87f919c375c
IamTanshu <sheshank@c-zentrix.com>
2021-04-12 16:24:44 +0530

    Notification channel Group

--- app/src/main/java/com/iamtanshu/chatappwebsocket/Notify/App.java
+++ app/src/main/java/com/iamtanshu/chatappwebsocket/Notify/App.java
@@ -3,6 +3,7 @@ package com.iamtanshu.chatappwebsocket.Notify;
 import android.app.Application;
 import android.app.Notification;
 import android.app.NotificationChannel;
+import android.app.NotificationChannelGroup;
 import android.app.NotificationManager;
 import android.os.Build;
 
@@ -12,6 +13,10 @@ public class App extends Application {
     // In actual application define the purpose of notifi. channel
     public static final String CHANNEL_1_ID = "channel1";
     public static final String CHANNEL_2_ID = "channel2";
+    public static final String CHANNEL_3_ID = "channel3";
+    public static final String CHANNEL_4_ID = "channel4";
+    public static final String GROUP_1_ID = "group1";
+    public static final String GROUP_2_ID = "group2";
 
     @Override
     public void onCreate() {
@@ -22,14 +27,21 @@ public class App extends Application {
     private void createNotificationChannel() {
         //todo: Check whether the device is above or equal to api level 26
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            long[] a = {100,100,100,100};
+            NotificationChannelGroup group1 = new NotificationChannelGroup(
+                    GROUP_1_ID,
+                    "Group 1"
+            );
+            NotificationChannelGroup group2 = new NotificationChannelGroup(
+                    GROUP_2_ID,
+                    "Group 2"
+            );
             NotificationChannel channel1 = new NotificationChannel(
                     CHANNEL_1_ID,
                     "Channnel 1",
                     NotificationManager.IMPORTANCE_HIGH
             );
             channel1.setDescription("This is channel 1");
-            channel1.setVibrationPattern(a);
+            channel1.setGroup(GROUP_1_ID);
 
             NotificationChannel channel2 = new NotificationChannel(
                     CHANNEL_2_ID,
@@ -37,10 +49,30 @@ public class App extends Application {
                     NotificationManager.IMPORTANCE_LOW
             );
             channel2.setDescription("This is channel 2");
+            channel2.setGroup(GROUP_1_ID);
+
+            NotificationChannel channel3 = new NotificationChannel(
+                    CHANNEL_3_ID,
+                    "Channnel 3",
+                    NotificationManager.IMPORTANCE_HIGH
+            );
+            channel3.setDescription("This is channel 3");
+            channel3.setGroup(GROUP_2_ID);
+
+            NotificationChannel channel4 = new NotificationChannel(
+                    CHANNEL_4_ID,
+                    "Channel 4",
+                    NotificationManager.IMPORTANCE_LOW
+            );
+            channel4.setDescription("This is channel 4");
 
             NotificationManager manager = getSystemService(NotificationManager.class);
+            manager.createNotificationChannelGroup(group1);
+            manager.createNotificationChannelGroup(group2);
             manager.createNotificationChannel(channel1);
             manager.createNotificationChannel(channel2);
+            manager.createNotificationChannel(channel3);
+            manager.createNotificationChannel(channel4);
 
         }
     }
